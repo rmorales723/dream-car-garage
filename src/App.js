20c6c9e19a6189972cce050f9a069236df44d3a3
robert morales <rmoralfit@gmail.com>
2021-06-28 20:26:31 -0400

    added <form/> and onChange function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import LandingPage from './components/LandingPage'
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import CarsContainer from './components/CarsContainer'
-
+import FormContainer from './components/FormContainer'
 
 function App() {
   return (
     <div className="App">
+      <form />
       <Router> 
         <Switch>
           <Route exact path= "/" component={LandingPage}/>
           <Route exact path= "/cars" component={CarsContainer}/>
-          <Route exact path= "/" component={FormContainer}/>
+          <Route exact path= "/cars/new" component={FormContainer}/>
         </Switch>
       </Router>
     </div>
--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class FormContainer extends React.Component {
+    state = {
+       make : ""
+    };
 
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+        console.log(this.state);
+    };
 
+    render() {
+        return(
+            <div>
+                <form>
+                    <input type="text" make="name" model=" " img=" "
+                        value={this.state.make}
+                        onChange={this.handleOnChange}
+                    />
+                    <button type="submit">Submit</button>
+                </form>   
+            </div>
+            
+        );
+    }
 
-
-
-
+}
 
 
 export default FormContainer;
